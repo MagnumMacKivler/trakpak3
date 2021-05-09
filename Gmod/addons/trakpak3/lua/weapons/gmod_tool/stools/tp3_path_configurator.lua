e74f4210e2d23c3e0a999438d23a4472d2acdbcb
MagnumMacKivler <76q309@gmail.com>
2021-05-09 16:54:05 -0400

    Update tp3_path_configurator.lua
    Mag is a big dumb baby

--- Gmod/addons/trakpak3/lua/weapons/gmod_tool/stools/tp3_path_configurator.lua
+++ Gmod/addons/trakpak3/lua/weapons/gmod_tool/stools/tp3_path_configurator.lua
@@ -17,29 +17,62 @@ if CLIENT then
 	Open Signal Config Menu (Right)
 	Deselect Signal (Reload)
 	
-	
 	]]--
-	TOOL.Information0 = {"left"}
 	
+	local SP = game.SinglePlayer()
+	
+	TOOL.Information0 = {"left"}
 	TOOL.Information1 = {"left", "right", "reload"}
+	TOOL.Information2 = {"left", "right", "reload"}
 	
 	language.Add("Tool.tp3_path_configurator.name","Trakpak3 Path Configurator")
-	language.Add("Tool.tp3_path_configurator.desc","Configure path info for signals. For use by mappers only!")
-	
-	function TOOL:SetupGui0()
-		self.Information = self.Information0
-		language.Add("Tool.tp3_path_configurator.left","Select a signal (master) and open the Path Configuration Menu.")
+	if SP then
+		language.Add("Tool.tp3_path_configurator.desc","Configure path info for signals. For use by mappers only!")
+	else
+		language.Add("Tool.tp3_path_configurator.desc","Configure path info for signals. This tool does not work in multiplayer!")
 	end
 	
-	function TOOL:SetupGui1()
-		self.Information = self.Information1
-		language.Add("Tool.tp3_path_configurator.left","Select a different signal, or toggle switch state for the selected path.")
-		language.Add("Tool.tp3_path_configurator.right","Open the Path Configuration Menu.")
-		language.Add("Tool.tp3_path_configurator.reload","Deselect the currently-selected signal.")
+	function TOOL:SetupGui(mode)
+		if not SP then
+			self.Information = {}
+		else
+			if mode==0 then --Nothing Selected
+				self.Information = self.Information0
+				language.Add("Tool.tp3_path_configurator.left","Select a tp3_signal_master and open its Path Configuration Menu.")
+			elseif mode==1 then --Signal Selected
+				self.Information = self.Information1
+				language.Add("Tool.tp3_path_configurator.left","Select a tp3_signal_master and open its Path Configuration Menu.")
+				language.Add("Tool.tp3_path_configurator.right","Open the Path Configuration Menu.")
+				language.Add("Tool.tp3_path_configurator.reload","Deselect the currently-selected signal.")
+			elseif mode==2 then --Signal and Path Selected
+				self.Information = self.Information2
+				language.Add("Tool.tp3_path_configurator.left","Select a NextSignal, Signal Block, or Logic Gate, or Select/Cycle a Switch.")
+				language.Add("Tool.tp3_path_configurator.right","Open the Path Configuration Menu.")
+				language.Add("Tool.tp3_path_configurator.reload","Deselect the currently-selected signal.")
+			end
+		end
 	end
 	
-	TOOL:SetupGui0()
+	TOOL:SetupGui(0)
 	
+	function TOOL:Think()
+		local newmode = 0
+		local sel = Trakpak3.PathConfig.selected
+		local path = Trakpak3.PathConfig.selected_path
+		
+		if sel and path then
+			newmode = 2
+		elseif sel then
+			newmode = 1
+		end
+		
+		if not self.mode then self.mode = 0 end
+		
+		if self.mode != newmode then
+			self.mode = newmode
+			self:SetupGui(newmode)
+		end
+	end
 	
 	function TOOL:Deploy()
 		
@@ -72,10 +105,12 @@ if CLIENT then
 					Trakpak3.PathConfig.SelectSignal(ent)
 				end
 			end
-		else --Hit World; Search for Blocks in area
+		else --Hit World; Search for Blocks in area or Deselect
 			--Trakpak3.PathConfig.DeselectSignal()
-			if use and Trakpak3.PathConfig.selected and Trakpak3.PathConfig.selected_path then
+			if Trakpak3.PathConfig.selected and Trakpak3.PathConfig.selected_path then
 				Trakpak3.PathConfig.SelectBlock()
+			elseif Trakpak3.PathConfig.selected then
+				Trakpak3.PathConfig.DeselectSignal()
 			end
 		end
 	end)
@@ -90,8 +125,155 @@ if CLIENT then
 		if Trakpak3.PathConfig.selected then Trakpak3.PathConfig.DeselectSignal() end
 	end)
 	
+	--Draw HUD Elements
 	function TOOL:DrawHUD()
 		Trakpak3.PathConfig.Draw()
+	end
+	
+	--Tool Menu
+	function TOOL.BuildCPanel(panel)
+		
+		--Save Button
+		local button = vgui.Create("DButton",panel)
+		button:SetText("Save Path Configs")
+		function button:DoClick()
+			LocalPlayer():EmitSound("buttons/button3.wav")
+			local AYS = vgui.Create("DFrame")
+			AYS:SetPos((ScrW() - 272)/2,(ScrH() - 128)/2)
+			AYS:SetSize(272,128)
+			AYS:SetTitle("Save Path Config File?")
+			AYS:MakePopup()
+			AYS:DockPadding(32,32,32,16)
+			
+			local warning = vgui.Create("DLabel",AYS)
+			warning:SetText("This will override the file in\ndata/trakpak3/pathconfig/.")
+			warning:SetSize(1,48)
+			warning:Dock(TOP)
+			
+			local yesbutton = vgui.Create("DButton", AYS)
+			yesbutton:SetText("Save")
+			yesbutton.DoClick = function()
+				Trakpak3.PathConfig.Save()
+				AYS:Close()
+			end
+			yesbutton:Dock(LEFT)
+			
+			local nobutton = vgui.Create("DButton", AYS)
+			nobutton:SetText("Cancel")
+			nobutton:SetColor(Color(255,0,0))
+			nobutton.DoClick = function()
+				AYS:Close()
+			end
+			nobutton:Dock(RIGHT)
+		end
+		panel:AddPanel(button)
+		
+		--Load (File) Button
+		local button = vgui.Create("DButton",panel)
+		button:SetText("Load From File")
+		button.DoClick = function()
+			
+			LocalPlayer():EmitSound("buttons/button3.wav")
+			local AYS = vgui.Create("DFrame")
+			AYS:SetPos((ScrW() - 272)/2,(ScrH() - 128)/2)
+			AYS:SetSize(272,128)
+			AYS:SetTitle("Load Path Config File?")
+			AYS:MakePopup()
+			AYS:DockPadding(32,32,32,16)
+			
+			local warning = vgui.Create("DLabel",AYS)
+			warning:SetText("This will override current path config data.")
+			warning:Dock(TOP)
+			
+			local yesbutton = vgui.Create("DButton", AYS)
+			yesbutton:SetText("Load")
+			yesbutton.DoClick = function()
+				Trakpak3.PathConfig.Load()
+				AYS:Close()
+			end
+			yesbutton:Dock(LEFT)
+			
+			local nobutton = vgui.Create("DButton", AYS)
+			nobutton:SetText("Cancel")
+			nobutton:SetColor(Color(255,0,0))
+			nobutton.DoClick = function()
+				AYS:Close()
+			end
+			nobutton:Dock(RIGHT)
+		end
+		panel:AddPanel(button)
+		
+		--Load (Map) Button
+		local button = vgui.Create("DButton",panel)
+		button:SetText("Load From Map")
+		button.DoClick = function()
+			
+			LocalPlayer():EmitSound("buttons/button3.wav")
+			local AYS = vgui.Create("DFrame")
+			AYS:SetPos((ScrW() - 272)/2,(ScrH() - 128)/2)
+			AYS:SetSize(272,128)
+			AYS:SetTitle("Load Path Config From Map?")
+			AYS:MakePopup()
+			AYS:DockPadding(32,32,32,16)
+			
+			local warning = vgui.Create("DLabel",AYS)
+			warning:SetText("This will override current path config data.")
+			warning:Dock(TOP)
+			
+			local yesbutton = vgui.Create("DButton", AYS)
+			yesbutton:SetText("Load")
+			yesbutton.DoClick = function()
+				Trakpak3.PathConfig.LoadFromServer(true)
+				AYS:Close()
+			end
+			yesbutton:Dock(LEFT)
+			
+			local nobutton = vgui.Create("DButton", AYS)
+			nobutton:SetText("Cancel")
+			nobutton:SetColor(Color(255,0,0))
+			nobutton.DoClick = function()
+				AYS:Close()
+			end
+			nobutton:Dock(RIGHT)
+		end
+		panel:AddPanel(button)
+		
+		--Clear Button
+		local button = vgui.Create("DButton",panel)
+		button:SetText("Clear Path Configs")
+		button.DoClick = function()
+			
+			LocalPlayer():EmitSound("buttons/button3.wav")
+			local AYS = vgui.Create("DFrame")
+			AYS:SetPos((ScrW() - 272)/2,(ScrH() - 128)/2)
+			AYS:SetSize(272,128)
+			AYS:SetTitle("Clear Path Configs?")
+			AYS:MakePopup()
+			AYS:DockPadding(32,32,32,16)
+			
+			local warning = vgui.Create("DLabel",AYS)
+			warning:SetText("Anything not saved will be lost.")
+			warning:Dock(TOP)
+			
+			local yesbutton = vgui.Create("DButton", AYS)
+			yesbutton:SetText("Clear")
+			yesbutton.DoClick = function()
+				Trakpak3.PathConfig.Clear()
+				AYS:Close()
+			end
+			yesbutton:Dock(LEFT)
+			
+			local nobutton = vgui.Create("DButton", AYS)
+			nobutton:SetText("Cancel")
+			nobutton:SetColor(Color(255,0,0))
+			nobutton.DoClick = function()
+				AYS:Close()
+			end
+			nobutton:Dock(RIGHT)
+
+		end
+		panel:AddPanel(button)
+		
 	end
 end
 
@@ -101,35 +283,49 @@ if SERVER then
 	util.AddNetworkString("tp3_pc_rightclick")
 	util.AddNetworkString("tp3_pc_reload")
 	
+	local SP = game.SinglePlayer()
+	
 	function TOOL:LeftClick(tr)
-		local ply = self:GetOwner()
-		--local json = util.TableToJSON(tr)
-		local id = 0
-		if not tr.HitWorld then id = tr.Entity:EntIndex() end
-		
-		local k_use = self:GetOwner():KeyDown(IN_USE)
-		
-		net.Start("tp3_pc_leftclick")
-		--net.WriteString(json)
-			net.WriteUInt(id,32)
-			net.WriteBool(k_use)
-		net.Send(ply)
-		return true
+		if SP then
+			local ply = self:GetOwner()
+			--local json = util.TableToJSON(tr)
+			local id = 0
+			if not tr.HitWorld then id = tr.Entity:EntIndex() end
+			
+			local k_use = self:GetOwner():KeyDown(IN_USE)
+			
+			net.Start("tp3_pc_leftclick")
+			--net.WriteString(json)
+				net.WriteUInt(id,32)
+				net.WriteBool(k_use)
+			net.Send(ply)
+			return true
+		else
+			return false
+		end
 	end
 	function TOOL:RightClick(tr)
-		local ply = self:GetOwner()
-		--local json = util.TableToJSON(tr)
-		net.Start("tp3_pc_rightclick")
-		--net.WriteString(json)
-		net.Send(ply)
-		return true
+		if SP then
+			local ply = self:GetOwner()
+			--local json = util.TableToJSON(tr)
+			net.Start("tp3_pc_rightclick")
+			--net.WriteString(json)
+			net.Send(ply)
+			return true
+		else
+			return false
+		end
 	end
 	function TOOL:Reload(tr)
-		local ply = self:GetOwner()
-		local json = util.TableToJSON(tr)
-		net.Start("tp3_pc_reload")
-		net.WriteString(json)
-		net.Send(ply)
-		return true
+		if SP then
+			local ply = self:GetOwner()
+			local json = util.TableToJSON(tr)
+			net.Start("tp3_pc_reload")
+			net.WriteString(json)
+			net.Send(ply)
+			return true
+		else
+			return false
+		end
 	end
 end
